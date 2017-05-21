c6f438f82d7a2db3cbc4c458add1c2e75515d023
Sebastian Konowrocki <sebastian@appetiq.com>
2017-05-21 19:21:56 +0200

    Add raw image parser

--- LungCancer/Form1.cs
+++ LungCancer/Form1.cs
@@ -8,5 +8,12 @@ namespace LungCancer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            var image = RawParser.Open(@"Images\JPCLN001.IMG");
+
+            pictureBox1.Image = image.Bitmap;
+        }
     }
 }
---
+++ LungCancer/RawParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace LungCancer
+{
+    static class RawParser
+    {
+        private const int DefaultImageSize = 2048;
+        private const int PixelDepth = 2;
+
+        public static Image<Gray, UInt16> Open(string fileName)
+        {
+            byte[] data = new byte[DefaultImageSize * DefaultImageSize * PixelDepth];
+
+            using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                fs.Read(data, 0, DefaultImageSize * DefaultImageSize * PixelDepth);
+            }
+
+            ushort[,,] imageData = new ushort[DefaultImageSize, DefaultImageSize, 1];
+            
+            for (int x = 0; x < DefaultImageSize; x++)
+            {
+                for (int y = 0; y < DefaultImageSize; y++)
+                {
+                    int index = (y * DefaultImageSize + x) * PixelDepth;
+                    ushort pixel = (ushort)(data[index] * 256 + data[index + 1]);
+
+                    imageData[y, x, 0] = pixel;
+                }
+            }
+
+            return new Image<Gray, UInt16>(imageData);
+        }
+    }
+}
